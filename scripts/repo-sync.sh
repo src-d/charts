f9e7c5fdfb9089d2b422e328d94f9e14208e4c83
Maartje Eyskens <maartje@eyskens.me>
2019-06-18 11:22:03 +0200

    Change repo-sync to work with new chart dirs
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- scripts/repo-sync.sh
+++ scripts/repo-sync.sh
@@ -4,7 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-REPO_URL="${REPO_URL:-https://src-d.github.io/charts/}"
+REPO_NAME="${REPO_NAME:-stable}"
+REPO_URL="${REPO_URL:-https://src-d.github.io/charts/$REPO_NAME/}"
 TARGET_BRANCH="${TARGET_BRANCH:-gh-pages}"
 GH_REF="${GH_REF:-github.com/src-d/charts.git}"
 GH_REMOTE_URL="https://$GITHUB_TOKEN@$GH_REF"
@@ -14,35 +15,43 @@ log_error() {
 }
 
 helm init --client-only
-helm repo add src-d "$REPO_URL"
+#helm repo add "srcd-$REPO_NAME" "$REPO_URL"
 
 packages_dir=$(mktemp -d)
+repo_packages_dir="$packages_dir/$REPO_NAME"
 index_dir=$(mktemp -d)
 trap "rm -rf $packages_dir $index_dir" EXIT
 
-pushd $packages_dir
+pushd "$packages_dir"
 git clone --branch=gh-pages --depth=1 "$GH_REMOTE_URL" .
 git config user.email "infra@sourced.tech"
 git config user.name "Infra sourced{d}"
+[ -d "$REPO_NAME" ] || mkdir "$REPO_NAME"
+pushd "$REPO_NAME"
+[ -d "index.yaml" ] || touch index.yaml
 mv index.yaml "$index_dir/index.yaml"
 popd
+popd
 
-for dir in $(ls | grep -vE '^scripts$|^docs$'); do
+pushd "$REPO_NAME"
+for dir in ./* ; do
     [ -d "$dir" ] || continue
     if helm dependency build "$dir"; then
-        helm package --destination "$packages_dir" "$dir"
+        helm package --destination "$repo_packages_dir" "$dir"
     else
         log_error "Problem building dependencies of '$dir'."
         exit 1
      fi
 done
+popd
 
-if ! helm repo index --url "$REPO_URL" --merge "$index_dir/index.yaml" "$packages_dir"; then
+if ! helm repo index --url "$REPO_URL" "$repo_packages_dir"; then # --merge "$index_dir/index.yaml"
     log_error "Exiting because unable to update index. Not safe to push update."
     exit 1
 fi
 
-cd $packages_dir
+cd "$packages_dir"
+cat "$repo_packages_dir/index.yaml"
 git add -A .
-git commit -sm 'Updating helm packages and index'
+git commit -sm "Updating helm packages and index for $REPO_NAME"
 git push "$GH_REMOTE_URL" "$TARGET_BRANCH:$TARGET_BRANCH"
